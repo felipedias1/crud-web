84ee8bc097929821aeb88b26c3e71d6375bbe371
Felipe Dias <felipeferreiradias1@gmail.com>
2022-05-30 08:25:39 -0300

    :sparkles: Build Customer Type Form

---
+++ src/components/TypeCustomer.jsx
@@ -0,0 +1,41 @@
+import React, {useContext} from "react";
+import DataContext from '../context/DataContext';
+
+function TypeCustomer(props) {
+  const { setType, type } = props;
+  const { edit } = useContext(DataContext);
+
+  return (
+    <div onChange={ ({ target }) => setType(target.value) }>
+      <label className='tipo'> Tipo </label>
+      <input 
+        disabled={edit["id"] && true}
+        type="radio" 
+        value="J" 
+        name="tipo"
+        checked={type==="J"}
+      /> Pessoa Jurídica
+      <input
+        disabled={edit["id"] && true}
+        type="radio" 
+        value="F" 
+        name="tipo"
+        checked={type==="F"}
+      /> Pessoa Física
+    </div>
+  );
+}
+
+export default TypeCustomer
+
+
+
+
+
+
+
+
+
+
+
+
