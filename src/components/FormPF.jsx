f27e6d63426ce634b3ab60f87e32f65434136d0b
Felipe Dias <felipeferreiradias1@gmail.com>
2022-05-31 04:49:16 -0300

    :bookmark: Style: add classes on pfform elements

--- src/components/FormPF.jsx
+++ src/components/FormPF.jsx
@@ -66,126 +66,150 @@ function FormPF() {
 
   return (
     <form>
-      <label className='nome-completo'>
-        Nome Completo
-        <input 
-          type="text" 
-          name="nome-completo" 
-          value={ nome_completo } 
-          onChange={ ({ target }) => setNomeCompleto(target.value) } 
-        />
-      </label>
-      <label className='apelido'>
-        Apelido
-        <input 
-          type="text" 
-          name="apelido" 
-          value={ apelido } 
-          onChange={ ({ target }) => setApelido(target.value) } 
-        />
-      </label>
-      <label className='cpf'>
-        CPF
-        <input 
-          type="text" 
-          name="cpf" value={ cpf } 
-          onChange={ ({ target }) => setCpf(target.value) } 
-        />
-      </label>
-      <label className='data-nascimento'>
-        Data de Nascimento
-        <input 
-          type="text" 
-          name="data-nascimento" 
-          value={ data_nascimento } 
-          onChange={({ target }) => setDataNascimento(target.value) } 
-        />
-      </label>
-      <label 
-        className='estado-civil'
-        onChange={ ({target}) => setEstadoCivil(target.value) }
-        >
-        Estado Civil
-        <select name="estado-civil" required>
-          <option value="s">Solteiro(a)</option>
-          <option value="c">Casado(a)</option>
-          <option value="d">Divorciado(a)</option>
-          <option value="v">Viúvo(a)</option>
-        </select>
-      </label>
-      <label className='rg'>
-        RG
-        <input 
-          type="text" 
-          name="rg" value={ rg } 
-          onChange={ ({ target }) => setRg(target.value) } 
-        />
-      </label>
-      <label className='orgao-emissor'>
-        Órgao Emissor
-        <input 
-          type="text" 
-          name="orgao-emissor" value={ orgao_emissor } 
-          onChange={ ({ target }) => setOrgaoEmissor(target.value) } 
-        />
-      </label>
-      <SelectUf rgUf={rg_uf} setRgUf={setRgUf} notAdress={true}/>
-      <label className='cnh'>
-        Cnh
-        <input 
-          type="text" 
-          name="cnh" value={ cnh } 
-          onChange={ ({ target }) => setCnh(target.value) } 
-        />
-      </label>
-      <label className='seguranca'>
-        Seguranca
-        <input 
-          type="text" 
-          name="seguranca" value={ seguranca } 
-          onChange={ ({ target }) => setSeguranca(target.value) } 
-        />
-      </label>
-      <label className='cei'>
-        CEI
-        <input 
-          type="text" 
-          name="cei" value={ cei } 
-          onChange={ ({ target }) => setCei(target.value) } 
-        />
-      </label>
-      <label className='email'>
-        Email
-        <input 
-          type="text" 
-          name="email" value={ email } 
-          onChange={ ({ target }) => setEmail(target.value) } 
-        />
-      </label>
-      <label className='telefone'>
-        Telefone
-        <input 
-          type="text" 
-          name="telefone" value={ telefone } 
-          onChange={ ({ target }) => setTelefone(target.value) } 
-        />
-      </label>
-      <label className='celular'>
-        Celular
-        <input 
-          type="text" 
-          name="celular" value={ celular } 
-          onChange={ ({ target }) => setCelular(target.value) } 
-        />
-      </label>
+      <div className="form-pf-part1">
+        <label className='nome-completo'>
+          Nome Completo
+          <input 
+            type="text" 
+            name="nome-completo" 
+            value={ nome_completo } 
+            onChange={ ({ target }) => setNomeCompleto(target.value) } 
+          />
+        </label>
+        <label className='apelido'>
+          Apelido
+          <input 
+            type="text" 
+            name="apelido" 
+            value={ apelido } 
+            onChange={ ({ target }) => setApelido(target.value) } 
+          />
+        </label>  
+      </div>
+      <div className="form-pf-part2">
+        <label className='cpf'>
+          CPF
+          <input 
+            type="text" 
+            name="cpf" value={ cpf } 
+            onChange={ ({ target }) => setCpf(target.value) } 
+          />
+        </label>
+        <label className='data-nascimento'>
+          Data de Nascimento
+          <input 
+            type="date" 
+            name="data-nascimento" 
+            value={ data_nascimento } 
+            onChange={({ target }) => setDataNascimento(target.value) } 
+          />
+        </label>
+        <label 
+          className='estado-civil'
+          onChange={ ({target}) => setEstadoCivil(target.value) }
+          >
+          Estado Civil
+          <select name="estado-civil" required>
+            <option value="s">Solteiro(a)</option>
+            <option value="c">Casado(a)</option>
+            <option value="d">Divorciado(a)</option>
+            <option value="v">Viúvo(a)</option>
+          </select>
+        </label>
+      </div>
+      <div className="form-pf-part3">
+        <label className='rg'>
+          RG
+          <input 
+            type="text" 
+            name="rg" value={ rg } 
+            onChange={ ({ target }) => setRg(target.value) } 
+          />
+        </label>
+        <label className='orgao-emissor'>
+          Órgao Emissor
+          <input 
+            type="text" 
+            name="orgao-emissor" value={ orgao_emissor } 
+            onChange={ ({ target }) => setOrgaoEmissor(target.value) } 
+          />
+        </label>
+        <SelectUf rgUf={rg_uf} setRgUf={setRgUf} notAdress={true}/>
+      </div>
+      <div className="form-pf-part4">
+        <label className='cnh'>
+          Cnh
+          <input 
+            type="text" 
+            name="cnh" value={ cnh } 
+            onChange={ ({ target }) => setCnh(target.value) } 
+          />
+        </label>
+        <label className='seguranca'>
+          Seguranca
+          <input 
+            type="text" 
+            name="seguranca" value={ seguranca } 
+            onChange={ ({ target }) => setSeguranca(target.value) } 
+          />
+        </label>
+        <label className='cei'>
+          CEI
+          <input 
+            type="text" 
+            name="cei" value={ cei } 
+            onChange={ ({ target }) => setCei(target.value) } 
+          />
+        </label>
+      </div>
+      <div className="form-pf-part5">
+        <label className='email'>
+          Email
+          <input 
+            type="text" 
+            name="email" value={ email } 
+            onChange={ ({ target }) => setEmail(target.value) } 
+          />
+        </label>
+      </div>
+      <div className="form-pf-part6">
+        <label className='telefone'>
+          Telefone
+          <input 
+            type="text" 
+            name="telefone" value={ telefone } 
+            onChange={ ({ target }) => setTelefone(target.value) } 
+          />
+        </label>
+        <label className='celular'>
+          Celular
+          <input 
+            type="text" 
+            name="celular" value={ celular } 
+            onChange={ ({ target }) => setCelular(target.value) } 
+          />
+        </label>
+      </div>
       <Endereco />
-      <button
-        type="button"
-        data-testid="button-create-customer"
-        onClick={ sendCustomerPayload }
-      >
-        Enviar
-      </button>
+      <div className="buttons">
+        <button
+          id="Enviar"
+          type="button"
+          data-testid="button-create-customer"
+          onClick={ sendCustomerPayload }
+        >
+          Enviar
+        </button>
+        <button
+          id="Cancelar"
+          type="button"
+          data-testid="button-create-customer"
+          onClick={ () => navigate('/') }
+        >
+          Cancelar
+        </button>
+      </div>
+      
     </form>
   );
 }
